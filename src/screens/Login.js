1b6c19dc89066de04fc4dd613e450f953679d869
jin <kyuj0810@gmail.com>
2024-03-26 23:02:57 +0900

    userForm1

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -11,6 +11,7 @@ import FormBox from '../components/auth/FormBox';
 import BottomBox from '../components/auth/BottomBox';
 
 import PageTitle from '../components/PageTitle';
+import { useForm } from 'react-hook-form';
 
 const FacebookLogin = styled.div`
   color: #385285;
@@ -21,6 +22,15 @@ const FacebookLogin = styled.div`
 `;
 
 function Login() {
+  const { register, handleSubmit } = useForm();
+  const onSubmitValid = (data) => {
+    console.log('onSubmitValid');
+    console.log(data);
+  };
+  const onSubmitInValid = (data) => {
+    console.log('onSubmitInValid');
+    console.log(data);
+  };
   return (
     <AuthLayout>
       <PageTitle title="Log in" />
@@ -29,9 +39,21 @@ function Login() {
           <FontAwesomeIcon icon={faCalendar} size="2x" />{' '}
           <span>My Routine</span>
         </div>
-        <form>
-          <Input type="text" placeholder="Username" />
-          <Input type="password" placeholder="Password" />
+        <form onSubmit={handleSubmit(onSubmitValid, onSubmitInValid)}>
+          <Input
+            {...register('username', {
+              required: 'Username is required.',
+              validate: (currentValue) => currentValue.includes('potato'),
+              minLength: 5,
+            })}
+            type="text"
+            placeholder="Username"
+          />
+          <Input
+            {...register('password', { required: 'Password is required.' })}
+            type="password"
+            placeholder="Password"
+          />
           <Button type="submit" value="Log in" />
         </form>
         <Separator />
